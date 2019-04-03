98d8ab3383265b4d3efc4a1db465580b7e233d30
Andrew Jones <joneseyit@protonmail.com>
2019-04-03 10:31:37 -0400

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,14 @@
-export default function manageUsers(state, action){
+let initialState = {
+    users: []
+}
+
+
+export default function manageUsers(state = initialState, action){
+    switch(action.type){
+        case 'ADD_USER':
+            let newState = {users: state.users.concat(action.user)}
+            return newState
+        default:
+            return state
+    }
 }
